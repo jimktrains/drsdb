0fe57e5f3b1c53f1b1a149d8d64248650176c623
Jim Keener <jim@jimkeener.com>
2017-04-12 03:26:42 -0400

    Using a slice instead of position

--- src/main.rs
+++ src/main.rs
@@ -14,28 +14,37 @@ struct TagValue<'a> {
 }
 
 #[derive(Clone)]
-struct ParserPosition {
-    position: usize,
+struct ParserPosition<'a> {
+    slice: &'a [u8],
     tag: u8,
     wire_type: PBWireType,
     cur_idx: u8,
     max_idx: u8,
 }
 
-fn PBParseNext<'a>(s: &'a [u8], pos: ParserPosition) -> (TagValue<'a>, ParserPosition) {
-    let idx2wireType = vec![PBWireType::VarInt,
+static IDX2WIRE_TYPE: &'static [PBWireType; 6] =
+                          &[PBWireType::VarInt,
                             PBWireType::Fixed64,
                             PBWireType::LengthDelim,
                             PBWireType::Reserved,
                             PBWireType::Reserved,
                             PBWireType::Fixed32];
+#[derive(Clone, Copy, Debug)]
+enum PBParseError {
+	UnknownWireType(usize)
+}
 
+fn PBParseNext<'a>(pos: ParserPosition<'a>) ->
+	Result<(TagValue<'a>, ParserPosition<'a>), PBParseError> {
     let mut p = pos.clone();
 
     if p.cur_idx == p.max_idx {
-        p.tag = s[p.position] >> 3;
-        p.wire_type = idx2wireType[(s[p.position] & 7) as usize];
-        p.position += 1;
+        p.tag = p.slice[0] >> 3;
+				let wire_type_idx = (p.slice[0] & 7) as usize;
+				if wire_type_idx >= IDX2WIRE_TYPE.len() {
+					return Err(PBParseError::UnknownWireType(wire_type_idx));
+				}
+        p.wire_type = IDX2WIRE_TYPE[wire_type_idx];
         p.cur_idx = 0;
         p.max_idx = 0;
     }
@@ -44,19 +53,20 @@ fn PBParseNext<'a>(s: &'a [u8], pos: ParserPosition) -> (TagValue<'a>, ParserPos
     }
     let mut value = match p.wire_type {
         PBWireType::LengthDelim => {
-            let offset = s[p.position] as usize;
-            let start = p.position + 1;
-            let end = p.position + offset;
-            p.position = end + 1;
-            &s[start..end + 1]
+            let offset = p.slice[1] as usize;
+            let start = 2;
+            let end = 2 + offset;
+            let x = &p.slice[start..end];
+            p.slice = & p.slice[end..];
+						x
         }
         _ => &[],
     };
-    (TagValue {
+    Ok((TagValue {
          value: value,
          tag: p.tag,
      },
-     p.clone())
+     p.clone()))
 }
 
 use std::str;
@@ -76,20 +86,20 @@ fn main() {
      */
     let msg = vec![0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67,
                    0x0a, 0x04, 0x74, 0x65, 0x73, 0x74];
+		let msg_slice = msg.as_slice();
 
-    let (mut x, mut pp) = PBParseNext(msg.as_slice(),
-                              ParserPosition {
-                                  position: 0,
+    let (mut x, mut pp) = PBParseNext(ParserPosition {
+                                  slice: msg_slice,
                                   tag: 0,
                                   wire_type: PBWireType::Reserved,
                                   cur_idx: 0,
                                   max_idx: 0,
-                              });
+                              }).unwrap();
 
     print!("Tag   = {}\n", x.tag);
     print!("Value = {}\n", str::from_utf8(x.value).unwrap());
 
-    let (x, pp) = PBParseNext(msg.as_slice(), pp);
+    let (x, pp) = PBParseNext(pp).unwrap();
 
     print!("Tag   = {}\n", x.tag);
     print!("Value = {}\n", str::from_utf8(x.value).unwrap());
